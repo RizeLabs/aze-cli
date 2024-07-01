5f1ee9a29bc521b8a508a0c4beed66837506fe6d
Rachit Chahar <202151123@iiitvadodara.ac.in>
2024-07-01 19:08:49 +0530

    tx submit retry logic

--- lib/src/executor/mod.rs
+++ lib/src/executor/mod.rs
@@ -1,43 +1,21 @@
 use crate::client::AzeClient;
 use miden_client::{
-    client::transactions::transaction_request::TransactionRequest, store::TransactionFilter,
+    client::transactions::{ TransactionResult, transaction_request::TransactionRequest },
+    store::TransactionFilter,
     errors::ClientError
 };
+use miden_objects::transaction::TransactionId;
 use miden_tx::{ TransactionExecutorError, TransactionCompilerError };
+use std::time::Duration;
 
 pub async fn execute_tx_and_sync(client: &mut AzeClient, tx_request: TransactionRequest) {
-    let _ = match client.sync_state().await {
-        Ok(_) => (),
-        Err(e) => {
-            println!("Error syncing state: {:?}", e);
-            return;
-        }
-    };
-    let transaction_execution_result = match client.new_transaction(tx_request.clone()) {
-        Ok(result) => result,
-        Err(e) => {
-            match e {
-                ClientError::TransactionExecutorError(TransactionExecutorError::CompileTransactionFailed(TransactionCompilerError::NoteIncompatibleWithAccountInterface(_))) => {
-                    // Suppress this specific error
-                    return;
-                }
-                _ => {
-                    // Handle other errors
-                    println!("Error creating transaction: {:?}", e);
-                    return;
-                }
-            };
-        }
-    };
-    let transaction_id = transaction_execution_result.executed_transaction().id();
+    sync_state_with_retry(client).await;
 
-    client
-        .submit_transaction(transaction_execution_result)
-        .await
-        .unwrap();
+    let transaction_id = submit_transaction_with_retry(client, tx_request.clone()).await;
+    
     // wait until tx is committed
     loop {
-        client.sync_state().await.unwrap();
+        sync_state_with_retry(client).await;
 
         // Check if executed transaction got committed by the node
         let uncommited_transactions = client
@@ -52,6 +30,54 @@ pub async fn execute_tx_and_sync(client: &mut AzeClient, tx_request: Transaction
             break;
         }
 
-        std::thread::sleep(std::time::Duration::new(3, 0));
+        std::thread::sleep(Duration::new(3, 0));
     }
+}
+
+async fn sync_state_with_retry(client: &mut AzeClient) {
+    for _try_number in 0..20 {
+        match client.sync_state().await {
+            Err(ClientError::NodeRpcClientError(_)) => {
+                std::thread::sleep(Duration::from_secs(2));
+            },
+            Err(other_error) => {
+                panic!("Unexpected error: {other_error}");
+            },
+            _ => return,
+        }
+    }
+    panic!("Failed to sync state");
+}
+
+async fn submit_transaction_with_retry(client: &mut AzeClient, tx_request: TransactionRequest) -> TransactionId {
+    for _try_number in 0..20 {
+        let transaction_execution_result = match client.new_transaction(tx_request.clone()) {
+            Ok(result) => result,
+            Err(e) => {
+                match e {
+                    ClientError::TransactionExecutorError(TransactionExecutorError::CompileTransactionFailed(TransactionCompilerError::NoteIncompatibleWithAccountInterface(_))) => {
+                        // Suppress this specific error
+                        break;
+                    }
+                    _ => {
+                        // Handle other errors
+                        println!("Error creating transaction: {:?}", e);
+                        break;
+                    }
+                };
+            }
+        };
+        let transaction_id = transaction_execution_result.executed_transaction().id();
+
+        match client.submit_transaction(transaction_execution_result).await {
+            Err(ClientError::NodeRpcClientError(_)) => {
+                std::thread::sleep(Duration::from_secs(2));
+            },
+            Err(other_error) => {
+                panic!("Unexpected error: {other_error}");
+            },
+            _ => return transaction_id,
+        }
+    }
+    panic!("Failed to submit transaction");
 }
